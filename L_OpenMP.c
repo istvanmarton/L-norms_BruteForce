e5de521036548fdbeef3a0388fbaafa15bbda045
istvanmarton <118180336+istvanmarton@users.noreply.github.com>
2025-06-26 15:09:49 +0200

    Add files via upload

--- L_OpenMP.c
+++ L_OpenMP.c
@@ -1,10 +1,3 @@
-/*****************************
-
-WRITTEN BY ISTVÁN MÁRTON
-
-*****************************/
-
-#include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
 #include <omp.h>
@@ -17,6 +10,41 @@ void calc_jmin_jmax(int* index, unsigned long long int* jMin, unsigned long long
 	else *jMax += *steps_remainder;
 	if(*index <= *steps_remainder) *jMin += *index;
 	else *jMin += *steps_remainder;
+}
+
+void LM(int_type* mtx_as_vec, unsigned long long int steps, unsigned long long int steps_remainder, int_type *L1_vector, int *L1_strategy, int iShorter, int iLonger){ // This function calculates the L1 norm.
+	int i, l, index, vect[NUM_OF_BITS - 1];
+	int_type temp[length], product, L1;
+	unsigned long long int number, jMax, jMin, iNumofZeros, aux;
+	index = omp_get_thread_num(); // Index of threads.
+	calc_jmin_jmax(&index, &jMin, &jMax, &steps, &steps_remainder); // This function calculates the minimal (jMin-th) and the maximal (jMax-th) word of the binary reflected Gray code for which the calculations must be performed by a given thread.
+	number = jMin;
+	for(l=0; l < iLonger; l++) {temp[l] = mtx_as_vec[(iShorter - 1) * iLonger + l];} // As the code can consider a row of the matrix with a fixed sign, it considers the last row of the matrix with +1.
+	product = 0;
+	for(i = 0 ; (iShorter - 1) > i; i++){
+		iNumofZeros=(unsigned long long int) 1 << i;
+		vect[i] = ((number+ iNumofZeros) >> (i+1)) & 1; // floor((j + 2^i)/2^(i+1)) Logical can be 0 and 1. logical is the number-th word and i-th digit of the BRGC.
+		if(vect[i] == 1){for(l=0; l < iLonger; l++){temp[l] += mtx_as_vec[i * iLonger + l]; }} // The code determines the vector-matrix multiplication belonging to the number-th word of the BRGC.
+		else {for(l=0; l < iLonger; l++){temp[l] -= mtx_as_vec[i * iLonger + l]; }}				
+	}
+	product += temp[0]; for(l = 1; l < iLonger; l++) { product += abs(temp[l]);} // The code calculates the L1 Bell value belonging to the number-th word of the BRGC.
+	L1 = product;
+	for(l=0; l< (iShorter - 1); l++){L1_strategy[index * (iShorter - 1) + l] = vect[l];} // The program stores the strategy vector belonging to the number-th BRGC word in the L1_strategy vector.
+
+	for(number=jMin + 1; number <= jMax; number++){ //The code determines the BRGC words until number variable reaches jMax.
+		product = 0;
+		aux = number;
+		for(i = 0; (aux & 1) == 0; i++){
+			aux = aux >> 1;
+		}
+		if(vect[i] == 0){vect[i] = 1; temp[0] += 2 * mtx_as_vec[i * iLonger]; product += temp[0]; for(l=1; l < iLonger; l++){temp[l] += 2 * mtx_as_vec[i * iLonger + l]; product += abs(temp[l]);}} // When the i-th digit is changed, the code changes the result of the vector-matrix multiplication. It only needs to deal with the i-th row of the matrix.
+		else {vect[i] = 0; temp[0] -= 2 * mtx_as_vec[i * iLonger]; product += temp[0]; for(l=1; l < iLonger; l++){temp[l] -= 2 * mtx_as_vec[i * iLonger + l]; product += abs(temp[l]);}}
+		if(product > L1) {
+			L1 = product; // If the current L1 sum, stored in product, is greater than the previous one, it modifies both the value 
+			for(l=0; l<(iShorter - 1); l++){L1_strategy[index * (iShorter - 1) + l] = vect[l];} // and the corresponding strategy vector as well.
+		}
+	}
+	L1_vector[index] = L1; // Every thread writes the biggest found L1 sum to L1_vector.
 }
 
 void L1(int_type* mtx_as_vec, unsigned long long int steps, unsigned long long int steps_remainder, int_type *L1_vector, int *L1_strategy, int iShorter, int iLonger){ // This function calculates the L1 norm.
@@ -210,40 +238,6 @@ void Ln(int_type* mtx_as_vec, int* iPattern, unsigned long long int steps, unsig
 	Ln_vector[index] = Ln; // Every thread writes the biggest found Ln sum to the Ln_vector.
 }
 
-void calc_Parameters(item* first, item_calc *second, int* num_of_threads){
-
-	if(first->n_original < second->iRows_reduced || first->n_original == 1) {second->n = first->n_original;}
-	else{
-		second->n = second->iRows_reduced > 1 ? second->iRows_reduced : 2;
-		printf("The preprocessed matrix has a number of rows (%d) less than or equal to the order of the L norm (%d).\n",second->iRows_reduced ,first->n_original);
-	}
-
-	if(second->n == 1){
-		first->total_num_to_calc = second->iRows_reduced == 0 ? 0 : (unsigned long long int) 1 << (second->iRows_reduced - 1); //The total number of L sums to be calculated is 2^(iRows_reduced-1)
-		second->maxRows = NUM_OF_BITS - 1;
-	}
-	else if(second->n == 2){
-		first->total_num_to_calc = second->iRows_reduced == 0 ? 0 : (unsigned long long int) 1 << (second->iRows_reduced - 1);
-		second->maxRows = NUM_OF_BITS - 1;
-	}
-	else if(second->n == 3){
-		first->total_num_to_calc = second->iRows_reduced == 0 ? 0 : pow(3, second->iRows_reduced - 1);
-		second->maxRows = (int) (floor (NUM_OF_BITS / log2(second->n)) + 1);
-	}
-	else{
-		first->total_num_to_calc = second->iRows_reduced == 0 ? 0 : pow(second->n, second->iRows_reduced - 1);
-		second->maxRows = (int) (floor (NUM_OF_BITS / log2(second->n)) + 1);
-	}
-	second->copyNum = *num_of_threads > first->total_num_to_calc ? first->total_num_to_calc : *num_of_threads; // copyNum is the actual number of threads. It cannot be more than the number of possible L values.
-	second->steps = second->copyNum == 0 ? 0 : first->total_num_to_calc/ second->copyNum;
-	second->steps_remainder = second->copyNum == 0 ? 0 : first->total_num_to_calc % second->copyNum;
-
-	printf("number of threads: %llu\n", second->copyNum);
-	printf("maximum length of strategy vector: %d\n", second->maxRows);
-	if(second->maxRows < second->iRows_reduced){printf("Matrix is too large. The matrix has %d rows after reduction. The maximum number of rows cannot be more than %d.\n", second->iRows_reduced, second->maxRows); exit(-1);}
-	if(second->iCols_reduced > length) {printf("Matrix is too large. The length variable %d should be greater or equal than %d.\n", length, second->iCols_reduced); exit(-1);}
-}
-
 void calc_Lnorm(item_calc* second){
 	int i, iMax, *Ln_strategy;
 	int_type *Ln_vector;
@@ -251,10 +245,17 @@ void calc_Lnorm(item_calc* second){
 	Ln_strategy = (int*) malloc(second->copyNum * (second->iRows_reduced - 1) * sizeof(int)); // The code allocates memory for the strategies belonging to the possible L sums.
 	second->strategy = (int*) calloc(second->iRows_reduced - 1, sizeof(int));
 
-	if(second->n == 1){ // If the order of the L norm is 1 then this part of the code will be executed.
+	if(second->n == 1 && second->marginal == 0){ // If the order of the L norm is 1 and we do not consider marginals then this part of the code will be executed.
+
 		#pragma omp parallel num_threads(second->copyNum)
 		{
 			L1(second->mtx_as_vec, second->steps, second->steps_remainder, Ln_vector, Ln_strategy, second->iRows_reduced, second->iCols_reduced); // The calculation of the L1 norm.
+		}
+	}
+	else if(second->n == 1 && second->marginal == 1){ // If the order of the L norm is 1 and we consider the marginals then this part of the code will be executed.
+		#pragma omp parallel num_threads(second->copyNum)
+		{
+			LM(second->mtx_as_vec, second->steps, second->steps_remainder, Ln_vector, Ln_strategy, second->iRows_reduced, second->iCols_reduced); // The calculation of the L1 norm.
 		}
 	}
 	else if(second->n == 2){ // If the order of the L norm is 2 then this part of the code will be executed.
@@ -325,7 +326,50 @@ void arguments_OMP(item* first, int* num_of_threads, int* argc, char** argv){
 	if(*argc < 5){first->stat = 'n';}
 	else {first->stat = argv[4][0];}
 	
+	if(*argc < 5 || first->n_original > 1){first->marginal = 0;}
+	else if(*argc == 5) {
+	  if(argv[4][0] == 'm' || argv[4][0] == 'M') {first->marginal = 1;}
+	  else {first->marginal = 0;}
+	}
+	else {
+	    if(argv[5][0] == 'm' || argv[5][0] == 'M') {first->marginal = 1;}
+	    else {first->marginal = 0;}
+	}
 	if(t > RANK_OF_NORM) {printf("The order of the L norm is too large. Please increase the RANK_OF_NORM variable in the code to %d and compile and run it again.\n", t); exit(-1);}
+}
+
+void calc_Parameters(item* first, item_calc *second, int* num_of_threads){
+
+	if(first->n_original < second->iRows_reduced || first->n_original == 1) {second->n = first->n_original;}
+	else{
+		second->n = second->iRows_reduced > 1 ? second->iRows_reduced : 2;
+		printf("The preprocessed matrix has a number of rows (%d) less than or equal to the order of the L norm (%d).\n",second->iRows_reduced ,first->n_original);
+	}
+
+	if(second->n == 1){
+		first->total_num_to_calc = second->iRows_reduced == 0 ? 0 : (unsigned long long int) 1 << (second->iRows_reduced - 1); //The total number of L sums to be calculated is 2^(iRows_reduced-1)
+		second->maxRows = NUM_OF_BITS - 1;
+	}
+	else if(second->n == 2){
+		first->total_num_to_calc = second->iRows_reduced == 0 ? 0 : (unsigned long long int) 1 << (second->iRows_reduced - 1);
+		second->maxRows = NUM_OF_BITS - 1;
+	}
+	else if(second->n == 3){
+		first->total_num_to_calc = second->iRows_reduced == 0 ? 0 : pow(3, second->iRows_reduced - 1);
+		second->maxRows = (int) (floor (NUM_OF_BITS / log2(second->n)) + 1);
+	}
+	else{
+		first->total_num_to_calc = second->iRows_reduced == 0 ? 0 : pow(second->n, second->iRows_reduced - 1);
+		second->maxRows = (int) (floor (NUM_OF_BITS / log2(second->n)) + 1);
+	}
+	second->copyNum = *num_of_threads > first->total_num_to_calc ? first->total_num_to_calc : *num_of_threads; // copyNum is the actual number of threads. It cannot be more than the number of possible L values.
+	second->steps = second->copyNum == 0 ? 0 : first->total_num_to_calc/ second->copyNum;
+	second->steps_remainder = second->copyNum == 0 ? 0 : first->total_num_to_calc % second->copyNum;
+
+	printf("number of threads: %llu\n", second->copyNum);
+	printf("maximum length of strategy vector: %d\n", second->maxRows);
+	if(second->maxRows < second->iRows_reduced){printf("Matrix is too large. The matrix has %d rows after reduction. The maximum number of rows cannot be more than %d.\n", second->iRows_reduced, second->maxRows); exit(-1);}
+	if(second->iCols_reduced > length) {printf("Matrix is too large. The length variable %d should be greater or equal than %d.\n", length, second->iCols_reduced); exit(-1);}
 }
 
 void load_parameters(item* first, item_calc* second, int* argc, char** argv, int* num_of_threads){
